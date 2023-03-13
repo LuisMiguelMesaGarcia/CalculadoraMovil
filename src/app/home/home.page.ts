1beaa297be528c5c0fb97b17fca1475101105dad
Luis Miguel Mesa <luismg1032@gmail.com>
2023-03-12 23:43:13 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  display = 0;
+  memory:any[]=[];
+  operator='';
+  operand=0;
 
   constructor() {}
+
+  numbers(n:any){
+    this.memory.push(n);
+    this.display=Number(this.memory.join(''))
+  }
+
+  dot(d:string){
+    if(this.memory.length === 0){
+
+    }else if (!this.memory.includes('.')){
+      this.memory.push(d);
+      this.display=Number(this.memory.join(''))
+    } 
+  }
+  
+  operation(op:string){
+    this.operand = this.display;
+    this.memory = [];
+    this.display=Number(this.memory.join(''))
+    this.operator= op;
+  }
+
+  equals(){
+    if(this.operator === '+'){
+      this.display += this.operand;
+      this.operand=0;
+      this.memory=[];
+    }else if (this.operator === '-'){
+      this.display = this.operand - this.display;
+      this.operand=0;
+      this.memory=[];
+    }else if (this.operator === 'x'){
+      this.display *= this.operand;
+      this.operand=0;
+      this.memory=[];
+    }else if (this.operator === '/'){
+      this.display = this.operand / this.display;
+      this.operand=0;
+      this.memory=[];
+    }else if (this.operator === '%'){
+      this.display = this.display * this.operand / 100;
+      this.operand=0;
+      this.memory=[];
+    }
+  }
+
+  clear(){
+    this.display=0;
+    this.memory=[];
+  }
+  back(){
+    this.memory.pop()
+    this.display=Number(this.memory.join(''))
+  }
 
 }
